ed8c929f1beac5091017571180a2dcd43c3e16b0
Anish Pradhan <anishpradha5523@gmail.com>
2023-11-15 02:22:47 +0530

    Ecommerce-Api

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
-const url ="mongodb+srv://Anishpradhan23:anish5523@cluster0.tu94oyn.mongodb.net/ecommerce?retryWrites=true&w=majority";
+const username = encodeURIComponent("Anishpradhan23")
+const password = encodeURIComponent("anish5523")
+const url =`mongodb+srv://${username}:${password}@cluster0.tu94oyn.mongodb.net/ecommerce?retryWrites=true&w=majority`;
+
+// const url ="mongodb+srv://Anishpradhan23:anish5523@cluster0.tu94oyn.mongodb.net/ecommerce?retryWrites=true&w=majority";
             const mongoose= require("mongoose")
             const connection =async()=>{
                 try{
                     await mongoose.connect(url)
                 }
                 catch(err){
-                    consolr.log("This is wrong",err)
+                    console.log("This is wrong",err)
                 }
             }
             module.exports =connection
