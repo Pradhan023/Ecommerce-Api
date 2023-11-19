a5e2c94d217ec4828da4c9d4a600c0d1d45da93a
Anish Pradhan <anishpradha5523@gmail.com>
2023-11-19 13:00:14 +0530

    Ecommerce-Api

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const cors = require('cors')
 const connection = require("./config/config")
 const route = require("./RouteComp/routeComp")
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(cors({
+    origin:"http://localhost:5173",
+}))
+
 app.use(express.json())
 
 app.use("/api",route)
